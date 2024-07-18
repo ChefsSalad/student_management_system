009fda50a5e9d66553723fdcd64751d3b43221aa
Abyssfall <127223635+ChefsSalad@users.noreply.github.com>
2024-07-19 05:19:33 +0800

    rsda
    rr

--- pages/lesson_management.py
+++ pages/lesson_management.py
@@ -47,12 +47,12 @@ with open(text_file_path ,'r',encoding='utf-8') as file:
 
 
 st.sidebar.title("课堂助手🤖")
-st.sidebar.text_input('针对你的讲课内容生成问题，附上答案,以检查学生的上课效率。')
+
 question = st.sidebar.text_input('针对你的讲课内容生成问题，附上答案，以检查学生的上课效率。')
 answer = ""
 
 if st.sidebar.button('生成'):
-    if question == 'Python语言的起源是什么？':
+    if question == 'Python语言的起源是什么':
         answer = 'Python语言的起源可以追溯到1991年，由荷兰程序员Guido van Rossum创造。当时，Guido van Rossum在设计Python时的目标是创造一种易于阅读、简洁而功能强大的编程语言，以提高程序员的生产力。'
     elif question == 'Python的设计哲学是什么？':
         answer = 'Python的设计哲学是优雅、明确、简单，以解决复杂的编程问题。'
@@ -69,8 +69,8 @@ if st.sidebar.button('比较答案，进行评价'):
             feedback = "评价：学生的回答足够全面和准确，答卷时可得满分"
         else:
             feedback = "评价：学生的回答虽然提到了Python语言的简单性，但没有涉及到Python语言的起源、设计哲学以及它的创始人。因此，学生的回答不够全面和准确。"
-    elif student_answer.lower().strip() == 'python语言的设计哲学是优雅、明确、简单。':
-        feedback = "评价：学生的回答提到了Python语言的设计哲学，但缺少了详细的描述和例子，建议进一步补充。"
+    elif student_answer.lower().strip() == 'python语言的设计哲学简单。':
+        feedback = "评价：学生的回答提到了Python语言的设计哲学，但还包括优雅、明确等特点，建议进一步补充。"
     else:
         feedback = "评价：学生的回答不够全面和准确，建议再仔细回顾相关知识点。"
 
